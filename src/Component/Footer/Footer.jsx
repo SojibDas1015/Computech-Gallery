e97483eb5a0c319c06fe7790551e1f7d8527f9f2
Sojib Das <sojibdas1015@gmail.com>
2026-03-19 22:08:31 +0600

    footer section color fix

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='py-20 text-center space-y-2 max-w-[95%] md:max-w-[84%] mx-auto'>
+        <div className='py-20 text-center space-y-2 max-w-[95%] md:max-w-[84%] mx-auto bg-white text-black'>
             <h2 className='text-black font-bold text-2xl text-center'><span className='font-normal'>Computech</span> Gallery</h2>
             <ul className='flex gap-5 justify-center items-center font-semibold'>
                 <li>Bid.</li>
