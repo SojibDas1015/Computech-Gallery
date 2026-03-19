53a01867c8e59ce6515c3c654f55f5f19ddddc93
Sojib Das <sojibdas1015@gmail.com>
2026-03-19 22:09:46 +0600

    footer background color white

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='py-20 text-center space-y-2 max-w-[95%] md:max-w-[84%] mx-auto bg-white text-black'>
-            <h2 className='text-black font-bold text-2xl text-center'><span className='font-normal'>Computech</span> Gallery</h2>
-            <ul className='flex gap-5 justify-center items-center font-semibold'>
-                <li>Bid.</li>
-                <li>Win.</li>
-                <li>Own.</li>
-            </ul>
-            <ul className='flex gap-5 items-center justify-center'>
-                <li>Home</li>
-                <li>Auctions</li>
-                <li>Categories</li>
-                <li>How to works</li>
-            </ul>
-            <p>© 2026 AuctionHub. All rights reserved.</p>
+        <div className=' bg-white'>
+            <div className='py-20 text-center space-y-2 max-w-[95%] md:max-w-[84%] mx-auto text-black'>
+                <h2 className='text-black font-bold text-2xl text-center'><span className='font-normal'>Computech</span> Gallery</h2>
+                <ul className='flex gap-5 justify-center items-center font-semibold'>
+                    <li>Bid.</li>
+                    <li>Win.</li>
+                    <li>Own.</li>
+                </ul>
+                <ul className='flex gap-5 items-center justify-center'>
+                    <li>Home</li>
+                    <li>Auctions</li>
+                    <li>Categories</li>
+                    <li>How to works</li>
+                </ul>
+                <p>© 2026 AuctionHub. All rights reserved.</p>
+            </div>
         </div>
     );
 };
