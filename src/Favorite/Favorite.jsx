ace04d67fbb99fda2431b44a1c6c6bf864b713e7
Sojib Das <sojibdas1015@gmail.com>
2026-03-19 21:46:26 +0600

    favorite card text black and remove card button bg red

--- src/Favorite/Favorite.jsx
+++ src/Favorite/Favorite.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Favorite = ({ product, handleRemoveFavorite }) => {
     return (
-        <tbody className='border-b-2 border-[#DCE5F3]'>
+        <tbody className='border-b-2 border-[#DCE5F3] text-black'>
             <tr>
                 <td rowSpan={2}><img className='w-20 p-2' src={product.image} alt="" /></td>
                 <td className=''>{product.title}</td>
-                <td onClick={()=> handleRemoveFavorite(product)} className='pl-10 cursor-pointer'>x</td>
+                <td onClick={()=> handleRemoveFavorite(product)} className='ml-10 cursor-pointer'><span className='bg-red-800 px-2 rounded-full font-semibold text-white'>X</span></td>
             </tr>
             <tr>
                 <td>${product.currentBidPrice}</td>
