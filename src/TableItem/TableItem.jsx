fb093ab0773146c2d8ef1e34e7237d527c1d624d
Sojib Das <sojibdas1015@gmail.com>
2026-03-19 21:07:34 +0600

    If you click on the button once, the button will be disabled

--- src/TableItem/TableItem.jsx
+++ src/TableItem/TableItem.jsx
@@ -8,10 +8,10 @@ const TableItem = ({ data, handleAddFavorite, products }) => {
             <td className='px-2 md:px-5 py-2 flex gap-2 items-center text-[12px] md:text-base'><img className='w-15 md:w-30' src={data.image} alt="" />{data.title}</td>
             <td className='px-2 md:px-5 py-2 text-[12px] md:text-base'>${data.currentBidPrice}</td>
             <td className='px-2 md:px-5 py-2 text-[12px] md:text-base'>{data.timeLeft}</td>
-            <td onClick={() => handleAddFavorite(data)} className='px-2 md:px-5 py-2 text-[12px] md:text-2xl font-bold cursor-pointer'><span className='flex justify-center'> {
+            <td onClick={() => handleAddFavorite(data)} className='px-2 md:px-5 py-2 text-[12px] md:text-2xl font-bold'><button disabled={active} className={`flex justify-center ${active ? 'cursor-not-allowed' : 'cursor-pointer'}`}> {
                 active ? <MdFavorite className='text-red-700'/> : <MdFavoriteBorder />
                 }
-            </span>
+            </button>
             </td>
         </tr>
     );
